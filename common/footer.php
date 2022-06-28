d07ca012504d8d498c5ea1eac6c681ba2ff29568
Mackenzie Brooks <mackenziekbrooks@gmail.com>
2022-06-28 14:09:06 -0400

    initial

---
+++ common/footer.php
@@ -0,0 +1,33 @@
+</div><!-- end content -->
+
+<footer role="contentinfo">
+
+        <div id="custom-footer-text">
+            <?php if ( $footerText = get_theme_option('Footer Text') ): ?>
+            <p><?php echo $footerText; ?></p>
+            <?php endif; ?>
+            <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
+                <p><?php echo $copyright; ?></p>
+            <?php endif; ?>
+        </div>
+
+        <a href="https://www.wlu.edu/arts/museums/"><img src="https://exhibits-museums.omeka.wlu.edu/files/theme_uploads/museums-logo-transparent.png" width="30%"> </a>
+
+    <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
+
+</footer>
+
+</div><!--end wrap-->
+
+<script type="text/javascript">
+jQuery(document).ready(function () {
+    Omeka.showAdvancedForm();
+    Omeka.skipNav();
+    Omeka.megaMenu("#top-nav");
+    Seasons.mobileSelectNav();
+});
+</script>
+
+</body>
+
+</html>
---
+++ common/header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html class="<?php echo get_theme_option('Style Sheet'); ?>" lang="<?php echo get_html_lang(); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php if ($author = option('author')): ?>
+    <meta name="author" content="<?php echo $author; ?>" />
+    <?php endif; ?>
+    <?php if ($copyright = option('copyright')): ?>
+    <meta name="copyright" content="<?php echo $copyright; ?>" />
+    <?php endif; ?>
+    <?php if ($description = option('description')): ?>
+    <meta name="description" content="<?php echo $description; ?>">
+    <?php endif; ?>
+
+    <?php
+    if (isset($title)) {
+        $titleParts[] = strip_formatting($title);
+    }
+    $titleParts[] = option('site_title');
+    ?>
+    <title><?php echo implode(' &middot; ', $titleParts); ?></title>
+
+    <?php echo auto_discovery_link_tags(); ?>
+
+    <!-- Plugin Stuff -->
+    <?php fire_plugin_hook('public_head', array('view'=>$this)); ?>
+
+    <!-- Stylesheets -->
+    <?php
+    queue_css_url('//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic');
+    queue_css_file(array('iconfonts', 'normalize', 'style'), 'screen');
+    queue_css_file('print', 'print');
+    echo head_css();
+    ?>
+
+    <!-- JavaScripts -->
+    <?php 
+    queue_js_file(array(
+        'vendor/selectivizr',
+        'vendor/jquery-accessibleMegaMenu',
+        'vendor/respond',
+        'jquery-extra-selectors',
+        'seasons',
+        'globals'
+    )); 
+    ?>
+
+    <?php echo head_js(); ?>
+</head>
+<?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
+    <a href="#content" id="skipnav"><?php echo __('Skip to main content'); ?></a>
+    <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
+    <div id="wrap">
+        <header role="banner">
+            <div id="site-title">
+                <?php echo exhibit_builder_link_to_exhibit($exhibit); ?>
+            </div>
+            <div id="site-image"><img src="https://exhibits-museums.omeka.wlu.edu/files/theme_uploads/hogarth-logo.png"></div>
+            
+            <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
+        </header>
+
+
+        <div id="content" role="main" tabindex="-1">
+            <?php
+                if(! is_current_url(WEB_ROOT)) {
+                  fire_plugin_hook('public_content_top', array('view'=>$this));
+                }
+            ?>
